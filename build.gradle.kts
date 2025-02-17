c2a09e73d91d74858ce5398fc3cd098d7083e6a9
Andre Weber <andre.weber3@etas.com>
2025-02-17 11:27:44 +0100

    chore: Generate gradle.lockfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ * Copyright (c) 2023 - 2025 Contributors to the Eclipse Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,6 +59,12 @@ subprojects {
             systemPropertiesMap[key.toString()] = value.toString()
         }
         systemProperties = systemPropertiesMap
+    }
+
+    // https://docs.gradle.org/current/userguide/dependency_locking.html
+    dependencyLocking {
+        lockAllConfigurations()
+        lockFile = file("$projectDir/gradle.lockfile")
     }
 }
 
--- kuksa-java-sdk/src/test/kotlin/org/eclipse/kuksa/connectivity/databroker/docker/SecureDataBrokerDockerContainer.kt
+++ kuksa-java-sdk/src/test/kotlin/org/eclipse/kuksa/connectivity/databroker/docker/SecureDataBrokerDockerContainer.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 - 2024 Contributors to the Eclipse Foundation
+ * Copyright (c) 2023 - 2025 Contributors to the Eclipse Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,7 +57,6 @@ class SecureDataBrokerDockerContainer(
                 "--tls-private-key", "$tlsMount/Server.key",
                 "--jwt-public-key", "$authenticationMount/jwt.key.pub",
             )
-        val toString = withCmd.toString()
         return withCmd
             .exec()
     }
